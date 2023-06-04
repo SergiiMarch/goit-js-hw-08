cf97b1fedc7448e7749c0feebb015a1d3d1926b9
Sergii Marchenko <marikmarchelo@gmail.com>
2023-06-04 23:02:09 +0300

    add task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+const onPlay = function (timeupdate) {
+  throttle(
+    localStorage.setItem('videoplayer-current-time', timeupdate.seconds),
+    1000
+  );
+};
+
+player.on('timeupdate', onPlay);
+
+if (localStorage.getItem('videoplayer-current-time') || 0) {
+  player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,16 @@
+import throttle from 'lodash.throttle';
+const formEl = document.querySelector('.feedback-form');
 
+const STORAGE_KEY = 'feedback-form-state';
+const inputForm = {
+  email: '',
+  message: '',
+};
+formEl.addEventListener('input', throttle(onInputData, 500));
+formEl.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+  e.preventDefault();
+  console.log(e.elements.email.value);
+}
+function onInputData(e) {}
