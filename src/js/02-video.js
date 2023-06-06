8e8b1046df48a636f67a29c3d281854b7ce97115
Sergii Marchenko <marikmarchelo@gmail.com>
2023-06-06 15:04:56 +0300

    tasck-2 виправив помилки

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,18 +1,18 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-const iframe = document.querySelector('#vimeo-player');
+const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-const onPlay = function (timeupdate) {
-  throttle(
-    localStorage.setItem('videoplayer-current-time', timeupdate.seconds),
-    1000
-  );
-};
+player.on(
+  'timeupdate',
+  throttle(function (data) {
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+  }, 1000)
+);
 
-player.on('timeupdate', onPlay);
+const getTimeVideo = localStorage.getItem('videoplayer-current-time');
 
-if (localStorage.getItem('videoplayer-current-time') || 0) {
-  player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+if (getTimeVideo || 0) {
+  player.setCurrentTime(getTimeVideo);
 }
